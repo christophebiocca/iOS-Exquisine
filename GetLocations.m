659590fa27853c59e60328413ed1c0b85f971d7d
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-27 07:36:17 -0500

    Added API Call to get all locations for restaurant.

---
+++ GetLocations.m
@@ -0,0 +1,35 @@
+//
+//  GetLocations.m
+//  AvocadoTest1
+//
+//  Created by Christophe Biocca on 12-01-27.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GetLocations.h"
+#import "APICallProtectedMethods.h"
+#import "Location.h"
+
+@implementation GetLocations
+
+@synthesize locations;
+
++(void)getLocationsForRestaurant:(NSInteger)restaurant success:(void(^)(id))success failure:(void(^)(id,NSError*))failure{
+    [self sendGETRequestForLocation:[NSString stringWithFormat:@"/customer/locations/%d/", restaurant] 
+                            success:success
+                            failure:failure];
+}
+
+-(void)postCompletionHook{
+    [super postCompletionHook];
+    if(![self error]){
+        NSMutableArray* locationsList = [NSMutableArray array];
+        for(NSDictionary* locDict in [[self jsonData] objectForKey:@"locations"]){
+            Location* location = [[Location alloc] initFromData:locDict];
+            [locationsList addObject:location];
+        }
+        locations = locationsList;
+    }
+}
+
+@end
