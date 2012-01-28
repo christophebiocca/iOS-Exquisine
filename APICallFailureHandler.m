ceba2d38a67b53158781728f4a89b8a893b7773e
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-28 00:47:22 -0500

    Network Failure handlers base stuff.

---
+++ APICallFailureHandler.m
@@ -0,0 +1,13 @@
+//
+//  APICallFailureHandler.m
+//  AvocadoTest1
+//
+//  Created by Christophe Biocca on 12-01-27.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "APICallFailureHandler.h"
+
+@implementation APICallFailureHandler
+
+@end
