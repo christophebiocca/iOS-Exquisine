99dcfc47e71d2f55b8e67721b85d9172e7f2a41d
Christophe Biocca <christophe.biocca@gmail.com>
2012-02-01 22:23:34 -0500

    Lunh checskum validation for credit card on the client side.

--- PaymentInfo.m
+++ PaymentInfo.m
@@ -8,17 +8,56 @@
 
 #import "PaymentInfo.h"
 
+// Form-like object to validate all values in real time.
 @implementation PaymentInfo
+
+static NSRegularExpression* creditCard;
+static NSRegularExpression* separator;
+
++(void)initialize{
+    if(!creditCard){
+        NSError* err = nil;
+        const NSString* pattern = @"\\A(\\d[- ]?){13,16}\\Z";
+        creditCard = [[NSRegularExpression alloc] initWithPattern:(NSString*) pattern options:0 error:&err];
+        NSAssert(!err, @"Incorrect regex (%@)? UNACCEPTABLE", pattern);
+        if(err){
+            NSLog(@"%@", err);
+        }
+    }
+    if(!separator){
+        NSError* err = nil;
+        const NSString* pattern = @"[- ]";
+        separator = [[NSRegularExpression alloc] initWithPattern:(NSString*) pattern options:0 error:&err];
+        NSAssert(!err, @"Incorrect regex (%@)? UNACCEPTABLE", pattern);
+        if(err){
+            NSLog(@"%@", err);
+        }
+    }
+}
 
 -(void)setCardnumber:(NSString *)number withValidationMessage:(NSString**)retErr{
     cardnumber = number;
-    NSCharacterSet* cardDivisers = [NSCharacterSet characterSetWithCharactersInString:@" -"];
-    for(NSString* block in [cardnumber componentsSeparatedByCharactersInSet:cardDivisers]){
-        for(NSString* number in [block componentsSeparatedByString:@""]){
-            NSLog(@"cc#: %@", number);
-        }
+    if(![creditCard firstMatchInString:cardnumber 
+                               options:NSMatchingAnchored 
+                                 range:NSMakeRange(0, [cardnumber length])]){
+        *retErr = @"Must be a credit card number.";
+        return;
     }
-    //*retErr = @"FAIL";
+    NSString* cleaned = [separator stringByReplacingMatchesInString:cardnumber 
+                                                            options:0 
+                                                              range:NSMakeRange(0, [cardnumber length]) 
+                                                       withTemplate:@""];
+    NSInteger sum = 0;
+    for(int i=[cleaned length]-1; i >= 0 ; --i) {
+        unichar c = [cleaned characterAtIndex:i];
+        DebugLog(@"#cc: %c", c);
+        NSInteger value = c - 48;
+        if(i%2) value *= 2;
+        sum += (value / 10) + (value % 10);
+    }
+    if(sum %= 10){
+        DebugLog(@"Invalid lunh checksum %d" % sum);
+    }
 }
 
 -(void)setCardholderName:(NSString *)name withValidationMessage:(NSString**)retErr{
