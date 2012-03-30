a2c68e78283478b5fa27eb67739a694ebeee17fa
Jake Nielsen <jake.k.nielsen@gmail.com>
2012-03-30 15:27:51 -0400

    Makes the app check for a network connection every time the app is opened iff the app has no cached data

--- AvocadoTest1/AppDelegate.m
+++ AvocadoTest1/AppDelegate.m
@@ -84,7 +84,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    
+    if (![[page appData] initialized]) {
+        [[page appData] initializeFromServer];
+    }
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
